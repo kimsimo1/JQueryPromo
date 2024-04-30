aa83440d53b3c4c1b4792549cda0f34b20571901
Kim Simonik <142694228+kimsimo1@users.noreply.github.com>
2024-04-30 20:56:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+/* Students: Please use this week's project for Week 7: Assignment 7: jQuery Plugin Promotional Site. 
+     You will need to replace the contents of this JavaScript file with your own work, 
+     and create any other files, if any, required for the assignment.
+     When you are done, be certain to submit the assignment in both Repl.it and Canvas to be graded. */
+new SimpleDropit($('#example')[0]);
